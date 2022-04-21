819128a5a7416ef62fee6e354d64302b07e0baa4
Яна Бубнова <bujhvh@mail.ru>
2022-04-21 10:40:11 +0300

    созданы шаблоны

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,7 +3,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts), 
+    path('', views.index, name='main'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'), 
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import render
 
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница, самая главная')
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {'title': title}
+    return render(request, template, context)
 
 
 # Страница с постами, отфильтрованными по группам 
-def group_posts(request, slug):
-    return HttpResponse('Посты')    
+def group_posts(request, slug): 
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {'title': title}
+    return render(request, template, context)  
